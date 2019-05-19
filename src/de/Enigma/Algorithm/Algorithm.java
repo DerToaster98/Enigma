84b349df3bc5b26125d4320fd192f57738d907ad
DerToaster <seiler.oli@web.de>
2019-05-19 12:24:17 +0200

    Revert "notes for base functionality"
    This reverts commit 3682ea381446c73339eacd1bd40494a6e011f3ee.

--- src/de/Enigma/Algorithm/Algorithm.java
+++ src/de/Enigma/Algorithm/Algorithm.java
@@ -2,11 +2,6 @@ package de.Enigma.Algorithm;
 
 public class Algorithm implements Runnable {
 
-	/*
-	 * Enthält Basis Algorithmus für Ver/Entschlüsselung
-	 * Außerdem ist eine Methode enthalten, welche überprüft, ob ein Schlüssel korrekt ist
-	 * Dies überprüft die "Steckbrett Sektion" des Schlüssels auf Duplikate
-	 */
 	public Algorithm() {
 		// TODO Auto-generated constructor stub
 	}
