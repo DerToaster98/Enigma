08e509f43b1d198c19d65e708bc583d9c8dadd5f
L301198 <lisabinkert73@gmail.com>
2019-05-19 12:18:26 +0200

    Kommentar hinzugefügt

--- src/de/Enigma/Algorithm/Algorithm.java
+++ src/de/Enigma/Algorithm/Algorithm.java
@@ -2,8 +2,15 @@ package de.Enigma.Algorithm;
 
 public class Algorithm implements Runnable {
 
+	//Überprüfung ob Schlüssel korrekt ist 
+	//AAA-XXX-X1;Y1-X2;Y2-...-X10;Y10 max zehn Buchstaben
+	//AAA = welche Walze an welcher Position steht z.B 3 2 1 
+	//XXX= Walzenstellung z.B 07 23 4, 
+	//X1;Y1 = Buchstabe X1 ist im Steckbrett mit Y2 verbunden z.B E;G
+	
 	public Algorithm() {
 		// TODO Auto-generated constructor stub
+		
 	}
 
 	@Override
--- src/de/Enigma/Algorithm/Decryptor.java
+++ src/de/Enigma/Algorithm/Decryptor.java
@@ -4,6 +4,14 @@ public class Decryptor extends Algorithm {
 
 	public Decryptor() {
 		// TODO Auto-generated constructor stub
+		//Überprüfung ob Schlüssel korrekt ist 
+		//AAA-XXX-X1;Y1-X2;Y2-...-X10;Y10 max zehn Buchstaben
+		//A = welche Position welche Walze hat z.B 3 2 1 
+		//XXX= Walzenstellung z.B 07 23 4, 
+		//X1;Y1 = Buchstabe X1 ist im Steckbrett mit Y2 verbunden z.B E;G
+		
+		
+		
 	}
 
 }
