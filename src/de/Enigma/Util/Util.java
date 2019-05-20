0c097092ace2bfbabe0136910a98e342bf9dad57
L301198 <lisabinkert73@gmail.com>
2019-05-20 18:54:15 +0200

    createCharArray Methode implementiert

--- src/de/Enigma/Util/Util.java
+++ src/de/Enigma/Util/Util.java
@@ -6,8 +6,19 @@ public class Util {
 		// TODO Auto-generated constructor stub
 	}
 
+	//wandelt String in char-Arrya um
+	//die Leerzeichen werden nicht in das char-Array übernommen
 	public static char[] createCharArray(String message){
-		return null;
+		
+		char[] c = new char[message.length()];
+		int j = 0;
+		for (int i = 0; i < message.length(); i++) {
+			if(message.charAt(i)!= ' ') {
+				c[j] = message.charAt(i);
+				j++;
+			}
+		}
+		return c;
 	}
 	
 	// Liefert position eines buchstaben in einem alphabet zurück !!von 1 bis 26!! Wenn -1: nicht im Alphabet
