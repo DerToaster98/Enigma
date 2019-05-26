9d6e8c9ff3dcea6e69da3e7e0ac28632a250b035
L301198 <lisabinkert73@gmail.com>
2019-05-26 18:03:42 +0200

    AlgorithmController überarbeitet, isKeyValid in Util

--- src/de/Enigma/Algorithm/Algorithm.java
+++ src/de/Enigma/Algorithm/Algorithm.java
@@ -36,13 +36,12 @@ import de.Enigma.Util.Enums.EMill;
  * Außerdem ist eine Methode enthalten, welche überprüft, ob ein Schlüssel korrekt ist
  * Dies überprüft die "Steckbrett Sektion" des Schlüssels auf Duplikate
  */
-public abstract class Algorithm {
+public class Algorithm {
     private EnigmaConfig conf;
     private AlgorithmController controller;
     private String key;
     private String[] metaData;
-    private char[] plugboard = new char[40];
-    private int counter = 0;
+
 
 
     public Algorithm() {
@@ -77,125 +76,7 @@ public abstract class Algorithm {
      * @return
      * @brief Überprüft ob der Schlüssel korrekt eingegeben wurde
      */
-    public boolean isKeyValid(String key) {
-
-        String[] parts = key.split("-");
-        if (checkUKW(parts[0])) {
-
-            if (checkMills(parts[1])) {
-                if (checkPosition(parts[2])) {
-
-                    for (int i = 3; i < parts.length; i++) {
-
-                        if (!checkLetter(parts[i])) {
-
-                            return false;
-                        }
-
-
-                    }
-                    return true;
-                }
-            }
-
-        }
-        return false;
-
-    }
-
-    private boolean checkLetter(String txt) {
-        char[] c = txt.toCharArray();
-        if (c.length == 3) {
-            System.out.println(c.length);
-            if (notInPlugboard(c[0]) && inAlphabet(c[0], true)) {
-
-                if (c[1] == ';') {
-
-                    if (notInPlugboard(c[2]) && inAlphabet(c[2], true)) {
-                        ;
-                        return true;
-                    }
-                }
-            }
-        }
-
-        return false;
-    }
-
-
-    private boolean notInPlugboard(char c) {
-        for (char value : plugboard) {
-            if (c == value) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private boolean inAlphabet(char c, boolean plugboardBool) {
-        for (EAlphabet alphabet : EAlphabet.values()) {
-            char lowerAlphabet = (char) (alphabet.getAsChar() + 32);
-            if (c == alphabet.getAsChar()) {
-                if (plugboardBool) {
-                    plugboard[counter] = c;
-                    counter++;
-                    plugboard[counter] = (char) (c + 32);
-                    counter++;
-                }
-            }
-            if (c == lowerAlphabet) {
-                if (plugboardBool) {
-                    plugboard[counter] = c;
-                    counter++;
-                    plugboard[counter] = (char) (c - 32);
-                    counter++;
-                }
-                return true;
-            }
-
-        }
-        return false;
-    }
-
-    private boolean checkPosition(String txt) {
-
-        char[] c = txt.toCharArray();
-        if (c.length == 3) {
-            return inAlphabet(c[0], false) && inAlphabet(c[1], false) && inAlphabet(c[2], false);
-        }
-
-        return false;
-    }
-
-    private boolean checkMills(String txt) {
-
-        char[] mill = txt.toCharArray();
-        if (mill.length == 3) {
-            for (int i = 1; i <= 5; i++) {
-                char x = (char) (i + '0');
-                if (mill[0] == x) {
-                    for (int j = 1; j <= 5; j++) {
-                        char y = (char) (j + '0');
-                        if (mill[1] == y && mill[1] != x) {
-                            for (int k = 1; k <= 5; k++) {
-                                char z = (char) (k + '0');
-                                if (mill[2] == z && mill[2] != y && mill[2] != x) {
-
-                                    return true;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-
-    private boolean checkUKW(String u) {
-        return u.equals("A") | u.equals("B") | u.equals("C");
-    }
+  
 
     /**
      * @brief Übergibt metaData an FileHandler()
--- src/de/Enigma/Util/Util.java
+++ src/de/Enigma/Util/Util.java
@@ -128,5 +128,130 @@ public class Util {
         Gson gson = new Gson();
         return gson.toJson(s);
     }
+    
+    private char[] plugboard = new char[40];
+    private int counter = 0;
+    
+    public boolean isKeyValid(String key) {
+
+        String[] parts = key.split("-");
+        if (checkUKW(parts[0])) {
+
+            if (checkMills(parts[1])) {
+                if (checkPosition(parts[2])) {
+
+                    for (int i = 3; i < parts.length; i++) {
+
+                        if (!checkLetter(parts[i])) {
+
+                            return false;
+                        }
+
+
+                    }
+                    return true;
+                }
+            }
+
+        }
+        return false;
+
+    }
+    
+
+    private boolean checkLetter(String txt) {
+        char[] c = txt.toCharArray();
+        if (c.length == 3) {
+            System.out.println(c.length);
+            if (notInPlugboard(c[0]) && inAlphabet(c[0], true)) {
+
+                if (c[1] == ';') {
+
+                    if (notInPlugboard(c[2]) && inAlphabet(c[2], true)) {
+                        ;
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+
+    private boolean notInPlugboard(char c) {
+        for (char value : plugboard) {
+            if (c == value) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean inAlphabet(char c, boolean plugboardBool) {
+        for (EAlphabet alphabet : EAlphabet.values()) {
+            char lowerAlphabet = (char) (alphabet.getAsChar() + 32);
+            if (c == alphabet.getAsChar()) {
+                if (plugboardBool) {
+                    plugboard[counter] = c;
+                    counter++;
+                    plugboard[counter] = (char) (c + 32);
+                    counter++;
+                }
+            }
+            if (c == lowerAlphabet) {
+                if (plugboardBool) {
+                    plugboard[counter] = c;
+                    counter++;
+                    plugboard[counter] = (char) (c - 32);
+                    counter++;
+                }
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    private boolean checkPosition(String txt) {
+
+        char[] c = txt.toCharArray();
+        if (c.length == 3) {
+            return inAlphabet(c[0], false) && inAlphabet(c[1], false) && inAlphabet(c[2], false);
+        }
+
+        return false;
+    }
+
+    private boolean checkMills(String txt) {
+
+        char[] mill = txt.toCharArray();
+        if (mill.length == 3) {
+            for (int i = 1; i <= 5; i++) {
+                char x = (char) (i + '0');
+                if (mill[0] == x) {
+                    for (int j = 1; j <= 5; j++) {
+                        char y = (char) (j + '0');
+                        if (mill[1] == y && mill[1] != x) {
+                            for (int k = 1; k <= 5; k++) {
+                                char z = (char) (k + '0');
+                                if (mill[2] == z && mill[2] != y && mill[2] != x) {
+
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private boolean checkUKW(String u) {
+        return u.equalsIgnoreCase("A") || u.equalsIgnoreCase("B") || u.equalsIgnoreCase("C");
+    }
+
 
 }
