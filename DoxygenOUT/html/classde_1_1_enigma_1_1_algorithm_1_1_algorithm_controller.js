7c168f0cb7b36116f33a61acdd272fdf725e2def
DerToaster <seiler.oli@web.de>
2019-05-29 13:31:45 +0200

    update doxy & readme

--- DoxygenOUT/html/classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller.js
+++ DoxygenOUT/html/classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller.js
@@ -1,7 +1,6 @@
 var classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller =
 [
     [ "AlgorithmController", "classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller.html#a5c0cadf27ef012a8a048006ee1f4e527", null ],
-
     [ "crypt", "classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller.html#adea7d47f817527047ed9feac6b5b0e95", null ],
     [ "getController", "classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller.html#a86e2b015f44a0f1c7301ba1bee274d78", null ],
     [ "getEnigmaConfig", "classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller.html#a4d91d2ab019ea85574865683806f0a96", null ],
@@ -11,5 +10,4 @@ var classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller =
     [ "key", "classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller.html#a31b84913983e59babed3222b29da0343", null ],
     [ "main", "classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller.html#a34c40755b3285ed8949567e742ee268f", null ],
     [ "text", "classde_1_1_enigma_1_1_algorithm_1_1_algorithm_controller.html#a8246205cad76a3b2988e4fce693623ff", null ]
-
 ];
