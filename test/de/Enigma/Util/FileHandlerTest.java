dfc86a14d3c645a5f1238ac1e1dec153fed57a0b
DerToaster <seiler.oli@web.de>
2019-06-03 12:48:57 +0200

    Test für append char

--- test/de/Enigma/Util/FileHandlerTest.java
+++ test/de/Enigma/Util/FileHandlerTest.java
@@ -5,6 +5,9 @@ package de.Enigma.Util;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
+
+import java.lang.reflect.Field;
 
 import org.junit.Test;
 
@@ -18,7 +21,41 @@ public class FileHandlerTest {
 
 	@Test
 	public void testAppendChar() {
-
+		try {
+			Field encodedTextField = FileHandler.class.getDeclaredField("encodedText");
+			
+			encodedTextField.setAccessible(true);
+			
+			String currVal = (String) encodedTextField.get(FileHandler.getFileHandler());
+			
+			FileHandler.getFileHandler().resetEncodedText();
+			
+			currVal = (String) encodedTextField.get(FileHandler.getFileHandler());
+			assertEquals("", currVal);
+			
+			FileHandler.getFileHandler().appendChar('H');
+			FileHandler.getFileHandler().appendChar('A');
+			FileHandler.getFileHandler().appendChar('L');
+			FileHandler.getFileHandler().appendChar('L');
+			FileHandler.getFileHandler().appendChar('O');
+			FileHandler.getFileHandler().appendChar(' ');
+			FileHandler.getFileHandler().appendChar('W');
+			FileHandler.getFileHandler().appendChar('E');
+			FileHandler.getFileHandler().appendChar('L');
+			FileHandler.getFileHandler().appendChar('T');
+			
+			currVal = (String) encodedTextField.get(FileHandler.getFileHandler());
+			assertEquals("HALLO WELT", currVal);
+			
+			encodedTextField.setAccessible(false);
+			
+		} catch(NoSuchFieldException nsfe) {
+			nsfe.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Test
