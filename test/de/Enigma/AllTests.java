10f1fe17fb3c2b6f08bd81db596474af5fb7c826
DerToaster <seiler.oli@web.de>
2019-06-03 19:11:41 +0200

    Klasse, welche alle Tests beinhaltet

---
+++ test/de/Enigma/AllTests.java
@@ -0,0 +1,28 @@
+package de.Enigma;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import de.Enigma.Algorithm.AlgorithmTest;
+import de.Enigma.Algorithm.EnigmaConfigTest;
+import de.Enigma.Algorithm.MillTest;
+import de.Enigma.Core.LogTest;
+import de.Enigma.Util.EnumsTest;
+import de.Enigma.Util.FileHandlerTest;
+import de.Enigma.Util.UtilTest;
+
+@RunWith(Suite.class)
+@SuiteClasses(
+		{
+			//Tests für das Algorithm Package
+			AlgorithmTest.class, EnigmaConfigTest.class, MillTest.class,
+			//Tests für das Core Package
+			LogTest.class,
+			//Tests für das Util Package
+			EnumsTest.class, FileHandlerTest.class, UtilTest.class}
+		)
+
+public class AllTests {
+
+}
